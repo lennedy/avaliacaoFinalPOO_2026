c012d0306ee6ef23c335e1012506dd61225efc9b
aluno22 <lennedys@yahoo.com.br>
2026-07-22 16:59:59 -0300

    Update BancoApp.py

--- BancoApp.py
+++ BancoApp.py
@@ -36,10 +36,10 @@ class BancoApp:
         ]
 
         self.contas = [
-            ContaCorrente(cliente1, 1001, 500, 500, 10),
-            ContaPoupanca(cliente2, 1002, 1000, 10),
-            ContaPoupanca(cliente1, 1003, 300, 2),
-            ContaCorrente(cliente4, 1004, 20, 500, 10),
+            ContaCorrente(cliente1, 1001, 500, 500, 10, 100, "Pacote Ouro"),
+            ContaPoupanca(cliente2, 1002, 1000, 10, 200, "Pacote Platina"),
+            ContaPoupanca(cliente1, 1003, 300, 2, 300, "Pacote Diamante"),
+            ContaCorrente(cliente4, 1004, 20, 500, 10, 400, "Pacote Mestre"),
         ]
 
         self.criar_interface()
